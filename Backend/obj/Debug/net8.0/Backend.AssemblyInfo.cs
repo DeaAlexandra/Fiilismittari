2bceea419067d4d14b6e0ad80ee052c141cc67f8
Eltsu22222 <56360185+Eltsu22222@users.noreply.github.com>
2024-11-25 09:32:02 +0200

    juu

--- Backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
+++ Backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1b670f1aa75e2c190a60c4c9d934bc57c2defed5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c3df6991d91aeb7558b09d257f8554227be66f21")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
