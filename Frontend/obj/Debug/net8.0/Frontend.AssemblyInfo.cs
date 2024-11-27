1d88108b4e9780c01d5610e327d64d0cfc076d74
Eltsu22222 <56360185+Eltsu22222@users.noreply.github.com>
2024-11-27 14:04:03 +0200

    juu

--- Frontend/obj/Debug/net8.0/Frontend.AssemblyInfo.cs
+++ Frontend/obj/Debug/net8.0/Frontend.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Frontend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1918b415ee715ed6dcff3f1768d31cb16334a496")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+00954173b8c380c581b75cd6c354010c9cf5ba00")]
 [assembly: System.Reflection.AssemblyProductAttribute("Frontend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Frontend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
